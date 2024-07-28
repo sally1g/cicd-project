0df336fb1d6f510f3e9b6297f642a77d574664f7
pj.aykwon <mango1g@naver.com>
2024-07-28 09:32:04 +0900

    [CI/CD] first commit

---
+++ cicd_project/src/test/java/com/cicd/cicd_project/CicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cicd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
