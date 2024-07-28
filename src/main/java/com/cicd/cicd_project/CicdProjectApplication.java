ca286646a341ee9f95ddb03b3126bcb4605b8f2d
sally1g <mango1g@naver.com>
2024-07-28 11:14:07 +0900

    test commit

--- src/main/java/com/cicd/cicd_project/CicdProjectApplication.java
+++ src/main/java/com/cicd/cicd_project/CicdProjectApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CicdProjectApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(CicdProjectApplication.class, args);
+
+
     }
 
 }
